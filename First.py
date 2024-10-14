33ecd71ec3070c717535666bd9bb8bfb4aebf9c3
SHREYA CHAKRABORTY <shreyachakraborty1320@gmail.com>
2024-10-14 23:20:26 +0530

    Python code attached

---
+++ First.py
@@ -0,0 +1,8 @@
+print("Hello")
+#1>>>>>>>>>>>>>writing a simple message
+
+#2>>>>>>>>>>>>Area of a rectangle
+length=int(input("Provide us the length of your rectangle: "))
+width=int(input("Provide us the width of a rectangle: "))
+area=length*width
+print("The area of your rectangle is: ",area)
